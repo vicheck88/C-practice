86b4e93696bc4c2f666a8618ec75631eaf08f6fc
vicheck88 <vicheck88@gmail.com>
2017-09-26 10:21:26 +0900

    170926 자료

---
+++ C 연습/170925 알고리즘.c
@@ -0,0 +1,250 @@
+﻿#if 0
+//Dessert
+//농부 존은 소들의 저녁식사 줄 세우는 새로운 방법을 개발 했다. N(1~15)마리의 소들을 순서대로 세워놓은 후, 각 소들 사이에 +, - , . 셋 중 1가지가 써져있는 냅킨을 배치해서 최종 결과가 0 이 되게 해야 하는 것이다. 점(.)이 써져있는 냅킨을 통해 더 큰 수를 만들 수 있게 된다. 아래와 같은 경우를 보자. (ps .이 써져있는 냅킨은 '공백'이라고 생각하면 된다.)
+//1 - 2 . 3 - 4 . 5 + 6 . 7
+//이와 같은 배치는 1-23-45+67 을 나타낸다. 결과는 0 이다. 10.11은 1011 로 해석된다.
+
+//버퍼를 두어 각 계산별로 맞는 행동을 취함
+//그 후 DFS를 통해 전체 경로를 탐색
+#include <stdio.h>
+int N;
+int ar[15] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+char br[15];
+int pow[2] = { 10, 100 };
+char sign[3] = { '+', '-', '.' };
+int cnt;
+int stop;
+
+int cal(int n, int m, char sign)
+{
+	switch (sign)
+	{
+	case '+': return n + m;
+	case '-': return n - m;
+	case '.':
+		if (n >= 0) return n*pow[m / 10] + m;
+		else return n*pow[m / 10] - m;
+	}
+}
+
+void DFS(int n, int sum, int buf)
+{
+	int i, tmp_sum, tmp_buf;
+	if (n + 1 >= N)
+	{
+		if (!(sum + buf))
+		{
+			if (cnt < 20)
+			{
+				printf("%d", 1);
+				for (i = 1; i < N; i++) printf(" %c %d", br[i], ar[i]);
+				printf("\n");
+			}
+			cnt++;
+		}
+		return;
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		br[n + 1] = sign[i];
+		if (i != 2)
+		{
+			tmp_sum = sum + buf;
+			tmp_buf = cal(0, ar[n + 1], sign[i]);
+		}
+		else
+		{
+			tmp_sum = sum;
+			tmp_buf = cal(buf, ar[n + 1], sign[i]);
+		}
+		DFS(n + 1, tmp_sum, tmp_buf);
+	}
+}
+
+
+
+int main(void)
+{
+	scanf("%d", &N);
+	DFS(0, 0, 1);
+	printf("%d", cnt);
+}
+#endif // 0
+
+#if 0
+//비행기게임
+//게임의 룰은 다음과 같다.
+//1)    비행기는 최초 맵의 최하단 중앙점에서 시작하게 된다. (행-13/열 – 3)
+//시작위치에 적이나 동전은 존재하지 않는다.
+//2)    비행기는 매 초마다 반드시 한 행 위로 올라가게 되는데, 이때 좌측상단, 상단, 우측상단 이렇게 세 방향 중 한 곳으로 이동 가능하다.
+//3)    비행기가 이동한 위치에 ‘동전’이 있을 경우, 점수 10점을 획득하게 된다.
+//4)    비행기가 이동한 위치에 ‘적’이 있을 경우, 점수가 -7점 감소한다.
+//5)    비행기는 처음에 폭탄을 하나 가지고 게임을 시작하게 되는데, 이 폭탄을 사용하면, 폭탄을 사용하려는 위치로부터 5행 이내의 적들은 모두 제거된다. (동전은 제거되지 않는다.)
+//6)    맵의 제일 윗줄로 올라가면 게임은 종료된다. 
+
+// n==13이므로 DFS가 가능(총 6진트리)
+// 6진 중 폭탄을 사용할 경우에는 따로 변수를 두어 가지치기
+#include <stdio.h>
+#define MAX(x,y) ((x)>(y)?(x):(y))
+char mat[20][20];
+int T;
+int max=-100;
+
+void DFS(int n, int nj, int score, int bomb, int flag)
+{
+	int score_tmp=score, bomb_tmp=bomb;
+	if (nj < 0 || nj >= 5) return;
+	if (n < 1)
+	{
+		max = MAX(score, max);
+		return;
+	}
+	
+	if (mat[n][nj] == '*') score_tmp = score + 10;
+	if (!flag && mat[n][nj]=='X')
+	{
+		DFS(n - 1, nj - 1, score_tmp, 5, 1);
+		DFS(n - 1, nj, score_tmp, 5, 1);
+		DFS(n - 1, nj + 1, score_tmp, 5, 1);
+	}
+	if (mat[n][nj] == 'X' && bomb<=1) score_tmp = score - 7;
+	if (flag) bomb_tmp=bomb-1;
+	DFS(n - 1, nj - 1, score_tmp, bomb_tmp, flag);
+	DFS(n - 1, nj, score_tmp, bomb_tmp, flag);
+	DFS(n - 1, nj + 1, score_tmp, bomb_tmp, flag);
+}
+
+int main(void)
+{
+	int i;
+	scanf("%d", &T);
+	while (T--)
+	{
+		for (i = 1; i <= 13; i++) scanf("%s", mat[i]);
+		DFS(13, 2, 0, 0, 0);
+		printf("%d\n", max);
+		max = -100;
+	}
+	return 0;
+}
+
+#endif
+
+#if 0
+//빙산
+//지구 온난화로 인하여 북극의 빙산이 녹고 있다. 빙산을 그림 1과 같이 2차원 배열에 표시한다고 하자. 빙산의 각 부분별 높이 정보는 배열의 각 칸에 양의 정수로 저장된다. 빙산 이외의 바다에 해당되는 칸에는 0이 저장된다. 그림 1에서 빈칸은 모두 0으로 채워져 있다고 생각한다.
+//행의 개수가 5이고 열의 개수가 7인 2차원 배열에 저장된 빙산의 높이 정보
+//빙산의 높이는 바닷물에 많이 접해있는 부분에서 더 빨리 줄어들기 때문에, 배열에서 빙산의 각 부분에 해당되는 칸에 있는 높이는 일년마다 그 칸에 동서남북 네 방향으로 붙어있는 0이 저장된 칸의 개수만큼 줄어든다. 단, 각 칸에 저장된 높이는 0보다 더 줄어들지 않는다. 바닷물은 호수처럼 빙산에 둘러싸여 있을 수도 있다. 따라서 그림 1의 빙산은 일년 후에 그림 2와 같이 변형된다.
+//그림 3은 그림 1의 빙산이 2년 후에 변한 모습을 보여준다. 2차원 배열에서 동서남북 방향으로 붙어있는 칸들은 서로 연결되어 있다고 말한다. 따라서 그림 2의 빙산은 한 덩어리이지만, 그림 3의 빙산은 세 덩어리로 분리되어 있다.
+//한 덩어리의 빙산이 주어질 때, 이 빙산이 두 덩어리 이상으로 분리되는 최초의 시간(년)을 구하는 프로그램을 작성하시오. 그림 1의 빙산에 대해서는 2가 답이다. 만일 전부 다 녹을 때까지 두 덩어리 이상으로 분리되지 않으면 프로그램은 0을 출력한다.
+
+//첫 줄에는 이차원 배열의 행의 개수와 열의 개수를 나타내는 두 정수 N과 M이 한 개의 빈칸을 사이에 두고 주어진다. N과 M은 3 이상 300 이하이다. 그 다음 N개의 줄에는 각 줄마다 배열의 각 행을 나타내는 M개의 정수가 한 개의 빈 칸을 사이에 두고 주어진다. 각 칸에 들어가는 값은 0 이상 10 이하이다. 배열의 첫 번째 행과 열, 마지막 행과 열에는 항상 0으로 채워진다.
+
+typedef struct
+{
+	int i, j;
+}Q_t;
+Q_t Q[1000000];
+int row, col;
+int mat[310][310];
+int tmp[310][310];
+int chk[310][310];
+int flag;
+int cnt;
+int num;
+int total;
+int dirow[4] = { -1, 1, 0, 0 };
+int dicol[4] = { 0, 0, -1, 1 };
+int r, c;
+
+int BFS(int i, int j)
+{
+	int ti, tj, ni, nj, k,num=0;
+	int wp =0, rp = 0;
+	Q[wp].i = i; Q[wp++].j = j;
+	chk[i][j] = flag; num++;
+	while (rp < wp)
+	{
+		ti = Q[rp].i; tj = Q[rp++].j;
+		for (k = 0; k < 4; k++)
+		{
+			ni = ti + dirow[k];
+			nj = tj + dicol[k];
+			if (ni<1 || ni>row || nj<1 || nj>col) continue;
+			if (!mat[ni][nj] || chk[ni][nj] == flag) continue;
+			Q[wp].i = ni; Q[wp++].j = nj; chk[ni][nj] = flag; num++;
+		}
+	}
+	return num;
+}
+
+void check(void)
+{
+	int i, j,k;
+	int ni, nj;
+	for (i = 1; i <= row; i++)
+	{
+		for (j = 1; j <= col; j++) tmp[i][j] = mat[i][j];
+	}
+
+	for (i = 1; i <= row; i++)
+	{
+		for (j = 1; j <= col; j++)
+		{
+			if (mat[i][j])
+			{
+				for (k = 0; k < 4; k++)
+				{
+					ni = i + dirow[k];
+					nj = j + dicol[k];
+					if (mat[ni][nj]) continue;
+					if (mat[i][j] && !tmp[ni][nj])
+					{
+						mat[i][j]--;
+						if (!mat[i][j]) total--;
+					}
+				}
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	int i, j,result;
+	scanf("%d %d", &row, &col);
+	for (i = 1; i <= row; i++)
+	{
+		for (j = 1; j <= col; j++)
+		{
+			scanf("%d", &mat[i][j]);
+			if (mat[i][j]) total++;
+		}
+	}
+	while (total)
+	{
+ 		cnt++;
+		check();
+		num = 0; flag++;
+		for (i = 1; i <= row; i++)
+		{
+			for (j = 1; j <= col; j++)
+			{
+				if (mat[i][j])
+				{
+					r = i; c = j; goto back;
+				}
+			}
+		}
+	back:
+		//result = BFS(r, c);
+		//if (result < total) break;
+		if (BFS(r, c) < total) break;
+	}
+	if (!total) printf("0");
+	else printf("%d", cnt);
+	return 0;
+}
+
+#endif
---
+++ 주교재/ARM 프로세서 구조 및 활용/3BIOS및부트코드설계.md
@@ -0,0 +1,110 @@
+# BIOS 및 부트코드 설계
+### 실습 시스템
+- SoC: 삼성 S3C2440A
+- 코어: ARM920T
+
+### 시스템 초기 설정 요소
+- 사용 가능하도록 최소한의 설정 작업 필요
+- WDT(Watchdog Timer)
+	- 시스템 고장 방지를 위한 시스템 감시 타이머
+	- 전원 인가시 Enable 상태로, 주기적으로 시스템을 리셋
+	- 따라서 주기적으로 카운터를 되돌려야 하며, 그렇지 않을 경우 문제점으로 인식하고 재부팅
+	- 사용준비가 되기 전까지 Disable 상태로 변환해야 함
+- PLL
+	- PLL은 CPU의 클락을 높이는 역할 수행
+	- 설정하지 않을 경우 낮은 주파수로 CPU 동작
+	- 활성화를 통해 CPU의 주파수를 높여야(FLCK)
+	- CPU 이외에도 여러 버스에 사용하기 위해 분주작업 또한 필요(HCLK,PLCK)
+	- FCLK,HCLK,PCLK의 비율은 1:3:6 또는 1:4:8로 설정
+- Interrupt
+	- 프로그램을 실행하는 도중 예기치 못한 상황이 발생하였을 때 발생한 상황을 먼저 처리한 뒤 원래 작업으로 복귀하는 것
+	- 초기화 작업중에는 아직 인터럽트 서비스 루틴이 생성되어있지 않음
+	- 인터럽트 컨트롤러에서 모든 인터럽트를 불가 상태로 만듦
+
+### ARM CPU의 기본구성
+- ARM 프로세서는 R0 ~ R15 레지스터와 CPSR 레지스터를 가지고 있음
+- RISC 계열 CPU들은 레지스터를 많이 두는 편
+- R0 ~ R14: 범용 레지스터로, 자유롭게 사용 가능
+	- R13,14 레지스터의 경우, 스택포인터, 함수 리턴값 저장용으로 사용되기도
+	- 따라서, R0 ~ R12의 경우만이 지정한 범용 레지스터
+- R15: 특수목적용 (프로그램 카운터)
+- CPSR: Current Program Status Register
+	- 현재 CPU의 상태 저장
+
+![ARM Register](http://infocenter.arm.com/help/topic/com.arm.doc.kui0049a/armregs.png)
+
+출처: http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.kui0049a/gs_ma_arm.htm
+
+### 레지스터
+- 하드웨어를 제어하기 위해서는 수많은 레지스터를 사용해야 함
+	- 레지스터: 하드웨어가 자기만 사용하기 위해 특별 용도로 예약한 메모리
+	- CPU 안 레지스터는 주소 없이 이름만 존재함
+- Port mapped IO
+	- CPU와 하드웨어가 별도의 정보를 주고 받기 위해 CPU와 하드웨어 사이에 직접 포트를 연결
+- Memory mapped IO
+    - 하드웨어 제어를 위한 주변장치들은 CPU 메모리 버스에 연결됨
+    - 각각의 주변장치들도 자신만의 레지스터가 있지만(TCON 등), CPU 입장에서는 범용 메모리에 불과
+    - 때문에, 모든 주변장치의 레지스터에는 주소가 배정되어있음
+	- 메모리 액세스를 통해 하드웨어의 레지스터를 제어하고, 이를 통해 결국 하드웨어를 제어
+
+![memory mapped io](https://i.stack.imgur.com/rBbh1.png)
+출처: https://superuser.com/questions/703695/difference-between-port-mapped-and-memory-mapped-access
+
+### Load, Store 개념
+- 레지스터를 제어하기 위해서는 어셈블리를 다루어야 함
+- MOV 명령
+	- ARM에서 연산은 레지스터 사이에서만 일어남
+	- MOV는 레지스터간 데이터 복사에 사용되는 명령(`MOV R0,R1`: R1 내용을 R0에 복사)
+- 주변장치 제어
+	- 연산이 레지스터 사이에서만 일어나므로, 외부 메모리의 자료를 레지스터로 가지고 올 수 있어야 함
+	- MOV 명령은 레지스터 사이에서만 가능한 명령이므로, 사용 불가
+	- 이 경우, Load, Store 명령 사용
+- Load, Store
+	- 레지스터와 외부 메모리 사이에 데이터를 이동할 때 사용
+	- 메모리 -> 레지스터: LDR(Load to Register)
+	- 레지스터 -> 메모리: STR(Store to Memory)
+
+### 상수 값 Load
+
+|LDR 명령 형식|`ldr Rn, =value`|
+|:---:|:---|
+||주어진 값을 Rn으로 복사<br/>Rn: R0 ~ R15로 대소문자 구분 없음<br/>`=value`: 상수값 지정, 최대 4바이트|
+|상수값 지정 방식|10진수: 41, -1, 16진수: 0x12 등|
+|활용 예|`LDR r0,=100`<br/>`ldr r1,=-200`<br/>`LDR R13,=0x1e3c`<br/>`ldr R15,=-0x1ff`|
+||R15에 로드: 다음 명령 주소 혹은 분기명령 의미|
+|Address label|label은 모두 주소 값이 됨(해당 label 위치의 주소)<br/>label은 첫 칸부터 기입|
+||`HERE: MOV r0, #10`<br/>`STR r0,[r1]`<br/>`LDR pc, =HERE`|
+||pc(R15)에 HERE명령의 주소(`MOV r0,#10`)를 기입: <br/>해당 명령을 다시 수행하며 결국 무한루프|
+
+### 레지터스 간접참조, LDR STR
+- 특정 주소에 있는 메모리를 LDR/STR할 때는 간접 참조 사용
+
+|레지스터 간접참조|`ldr Rd,[Rs]`|
+|:---:|:---|
+||`Rd := [Rs]`<br/>Rs 레지스터 값을 메모리 주소로 그 내용을 Rd로 로드<br/>Rs,Rd: r0 ~ r15 |
+||`str Rs,[Rd`|
+||`[Rd] := Rs`<br/>Rd 레지스터 값을 메모리 주소로 그 내용에 Rs 저장<br/>Rs,Rd: r0 ~ r15|
+|활용 예|`LDR r0,[r1]`: R0 값 = R1 주소 안의 값<br/>`STR r0,[r1]` : R0 값 = R1 주소 |
+
+- C문법으로 다음과 같이 이해가능	
+
+```cpp
+LDR Rd,[Rs]: Rd = *Rs;
+STR Rs,[Rd]: *Rd = Rs;
+```
+
+### WDT 레지스터
+- WDT를 Disable하려면 WTCON 레지스터의 비트 필드 설정을 바꾸어야
+- WDT는 Periperal로, 따라서 WTCON 레지스터는 메모리로 인식, CPU에서 접근
+
+![WTCON](http://www.ntrexgo.com/wp-content/uploads/2013/05/18feajkarm001.jpg)
+
+출처: http://www.ntrexgo.com/archives/13612
+
+### WDT 제어
+- 0x53000000번지에 0을 대입
+```llvm
+ldr r0,=0x53000000
+ldr r1,=0x0
+str r1,[r0]
+```
